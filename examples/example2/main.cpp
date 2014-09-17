f21fd86e9c91f748787c9cfd7e795efc42875fe3
Michael Adam <michaeladam3010@gmail.com>
2014-09-17 13:14:46 +0200

    use range based for loop

--- examples/example2/main.cpp
+++ examples/example2/main.cpp
@@ -6,16 +6,13 @@ int main() {
 	extension_system::ExtensionSystem extensionSystem;
 	extensionSystem.searchDirectory(".");
 
-	std::vector<extension_system::ExtensionDescription> interface2Extensions = extensionSystem.extensions<Interface2>();
-	std::vector<extension_system::ExtensionDescription>::iterator iter;
-
 	// iterate through all known Interface2 extensions and print metadata information
-	for( iter = interface2Extensions.begin(); iter != interface2Extensions.end(); iter++ ) {
-		std::cout<<"Extension: " << iter->name() << "(" << std::to_string(iter->version()) << ")" << std::endl;
-		std::cout<<"Description: " << iter->description() << std::endl;
-		std::cout<<"Author: "<< iter->getExtended()["author"] << std::endl;
-		std::cout<<"Vendor: "<< iter->getExtended()["vendor"] << std::endl;
-		std::cout<<"Target: "<< iter->getExtended()["target_product"] << std::endl;
+	for( auto extension : extensionSystem.extensions<Interface2>()) {
+		std::cout<<"Extension: " << extension.name() << "(" << std::to_string(extension.version()) << ")" << std::endl;
+		std::cout<<"Description: " << extension.description() << std::endl;
+		std::cout<<"Author: "<< extension.getExtended()["author"] << std::endl;
+		std::cout<<"Vendor: "<< extension.getExtended()["vendor"] << std::endl;
+		std::cout<<"Target: "<< extension.getExtended()["target_product"] << std::endl;
 	}
 
 	// create extension
