6c27a91723a357cd4422f9fb368a8a095606eabd
Bernd Amend <bernd.amend@gmail.com>
2019-12-13 23:29:45 +0100

    use SPDX license identifier

--- test/extension.cpp
+++ test/extension.cpp
@@ -1,11 +1,5 @@
-/**
-    @file
-    @copyright
-        Copyright Bernd Amend and Michael Adam 2014
-        Distributed under the Boost Software License, Version 1.0.
-        (See accompanying file LICENSE_1_0.txt or copy at
-        http://www.boost.org/LICENSE_1_0.txt)
-*/
+/// SPDX-FileCopyrightText: 2014-2020 Bernd Amend and Michael Adam
+/// SPDX-License-Identifier: BSL-1.0
 #include "Interfaces.hpp"
 #include <iostream>
 
